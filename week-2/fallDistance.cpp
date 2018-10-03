18694dd7e6c7d251aee8a3a2417154d99b1bb34e
Jedeu <jed.piezas@gmail.com>
2018-10-03 10:52:25 -0700

    Add fallDistance.cpp

---
+++ week-2/fallDistance.cpp
@@ -0,0 +1,19 @@
+/********************************************************************************* 
+ **Author: Jed Piezas
+ **Date: October 1 2018
+ **Description:  Calculates the distance an object has fallen. 
+ **              Input: @parameter t for time spent falling
+ **              Output: distance the object has fallen given time and gravity
+ **********************************************************************************/
+
+#include <iostream>
+#include <cmath>
+
+using std::pow;
+
+double fallDistance( double time )
+{
+    const double GRAVITY = 9.8;
+    return (GRAVITY * pow(time, 2))/2;
+}
+
