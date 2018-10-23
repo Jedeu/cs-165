d88c6b35b4d2835253e9a5eff2b349ed02f98a6a
Jedeu <jed.piezas@gmail.com>
2018-10-22 22:15:45 -0700

    Add comments

--- week-5/binarySearch/personSearch.cpp
+++ week-5/binarySearch/personSearch.cpp
@@ -17,17 +17,23 @@ int personSearch(const vector<Person>& persons, string name)
     int first = 0;
     int last = persons.size() - 1;
     int middle;
+    // Set to -1 to show that the person is not in the sorted vector
     int position = -1;
     bool found = false;
-
+    
+    // Loop while not found and we haven't exhausted the vector
     while (!found && first <= last)
     {
         middle = (first + last) / 2;
+        // If the middle vector's name is the person we're looking for
+        // Assign the position to middle and stop the loop
         if (persons[middle].getName() == name) {
             found = true;
             position = middle;
+        // Otherwise we either loop to the left of middle if we overshot
         } else if (persons[middle].getName() > name) {
             last = middle - 1;
+        // Or loop to the right if the name to search comes later in the vector
         } else {
             first = middle + 1;   
         }
