56900d9b4b71be7b42eae5e8c80082ef06ce8f23
Jedeu <jed.piezas@gmail.com>
2018-10-22 22:12:28 -0700

    Use Person class from week 4

--- week-5/binarySearch/Person.cpp
+++ week-5/binarySearch/Person.cpp
@@ -1,9 +1,9 @@
 /************************************************************************************ 
- ***** Author: Jed Piezas
- ***** Date: October 21 2018
- ***** Description:  Contains the implementation details of the Person class.
- *****               Uses prototypes in the Person header file.
- *************************************************************************************/
+ ****** Author: Jed Piezas
+ ****** Date: October 15 2018
+ ****** Description:  Contains the implementation details of the Person class.
+ ******               Uses prototypes in the Person header file.
+ **************************************************************************************/
 
 #include "Person.hpp"
 #include <string>
@@ -13,21 +13,27 @@ using std::string;
 Person::Person()
 {
     name = "";
+    age = 0.0;
 }
 
 /**********************************************
  *** This is the constructor for the Person
  *** when parameters are passed in
- *** Sets name based on specified
+ *** Sets name and age based on specified
  *** parameter.
  **********************************************/
-
-Person::Person(string newName)
+Person::Person(string newName, double newAge)
 {
-    name = newName; 
+    name = newName;
+    age = newAge; 
 }
 
 string Person::getName() const
 {
     return name;
+}
+
+double Person::getAge()
+{
+    return age;
 }
--- week-5/binarySearch/Person.hpp
+++ week-5/binarySearch/Person.hpp
@@ -1,26 +1,31 @@
 /************************************************************************************ 
- ****** Author: Jed Piezas
- ****** Date: October 21 2018
- ****** Description:  Header file for the Person class. It contains a constructor to 
- ******               initialize their name
- ******               It also defines the prototype for getter methods to its data 
- ******               members.
- **************************************************************************************/
+****** Author: Jed Piezas
+****** Date: October 15 2018
+****** Description:  Header file for the Person class. It contains a constructor to 
+******               initialize their name and age.
+******               It also defines the prototype for getter methods to its data 
+******               members.
+**************************************************************************************/
 
 #include <string>
 
 using std::string;
 
+//include guards for the header
 #ifndef PERSON_HPP
 #define PERSON_HPP
 
 class Person
 {
+    // Private data members
     private:
         string name;
+        double age;
+    // Constructor and getter methods
     public:
         Person();
-        Person(string);
+        Person(string, double);
         string getName() const;
+        double getAge();
 };
 #endif
