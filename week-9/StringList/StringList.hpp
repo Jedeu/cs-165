005cfa6af702c5d5ab2c852cd085a3ca19b0eaec
Jedeu <jed.piezas@gmail.com>
2018-11-18 21:23:27 -0800

    Add header file for StringList

---
+++ week-9/StringList/StringList.hpp
@@ -0,0 +1,42 @@
+/**************************************************************************************** 
+ ******** Author: Jed Piezas
+ ******** Date: November 18 2018
+ ******** Description:  Header file for the StringList class. It contains a copy
+ ********               constructor, destructor, and a default constructor.
+ ********               It also has methods that help with manipulating the StringList
+ ****************************************************************************************/
+
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
+
+#ifndef STRINGLIST_HPP
+#define STRINGLIST_HPP
+
+class StringList
+{
+    private:   
+        struct ListNode {
+                string value;
+                ListNode *next;
+            ListNode(string value, ListNode *next = nullptr)
+            {
+                this->value = value;
+                this->next = next;
+            }
+        }
+        // List head pointer
+        ListNode *head; 
+    public:
+        StringList();
+        // Destructor
+        ~StringList();
+        // Copy constructor
+        StringList(StringList& );
+        void add(string);
+        int positionOf(string);
+        bool setNodeVal(int, string);
+        vector<string> getAsVector(); 
+}
